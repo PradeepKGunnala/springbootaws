a281bc2a5413234014276dcc0c92bed410ede113
pradeepkgunnala <cloudpolyglot@gmail.com>
2023-10-19 19:18:55 -0400

    CompletionService - to return values which ever tasks finishes first
    CompletionService - to return values which ever tasks finishes first

--- src/main/java/com/springbootaws/Threads/JP18ReturningValuesUsingExecutor_FirstTechnique.java
+++ src/main/java/com/springbootaws/Threads/JP18ReturningValuesUsingExecutor_FirstTechnique.java
@@ -1,9 +1,10 @@
 package com.springbootaws.Threads;
 
+import java.util.concurrent.CompletionService;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorCompletionService;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 
 public class JP18ReturningValuesUsingExecutor_FirstTechnique {
 
@@ -13,25 +14,26 @@ public class JP18ReturningValuesUsingExecutor_FirstTechnique {
 		String currentThread = Thread.currentThread().getName();
 		System.out.println("currentThread :: " + currentThread);
 		ExecutorService execService = Executors.newCachedThreadPool();
-		Future<Integer> result1 = execService.submit(new JP19CalculateTaskA(2, 3, 2000));
-		Future<Integer> result2 = execService.submit(new JP19CalculateTaskA(4, 5, 1000));
-		Future<Integer> result3 = execService.submit(new JP19CalculateTaskA(5, 7, 500));
-		Future<?> result4 = execService.submit(new JP05LoopTaskA());
-		Future<Double> result5 = execService.submit(new JP05LoopTaskA(), 99.908);
-		execService.shutdown();
-		try {
-			System.out.println("Result -1 = " + result1.get());
-			System.out.println("Result -2 = " + result2.get());
-			System.out.println("Result -3 = " + result3.get());
-			System.out.println("Result -4 = " + result4.get());
-			System.out.println("Result -5 = " + result5.get());
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (ExecutionException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+
+		CompletionService<Integer> completionService = new ExecutorCompletionService<Integer>(execService);
+		completionService.submit(new JP19CalculateTaskA(2, 3, 2000));
+		completionService.submit(new JP19CalculateTaskA(4, 5, 1000));
+		completionService.submit(new JP19CalculateTaskA(5, 7, 500));
+		// Future<?> result4 = execService.submit(new JP05LoopTaskA());
+		completionService.submit(new JP05LoopTaskA(), 99);
+
+		for (int i = 0; i < 4; i++) {
+			try {
+				System.out.println("Result" + completionService.take().get());
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (ExecutionException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
+		execService.shutdown();
 		System.out.println("currentThread :: End" + currentThread);
 	}
 }
